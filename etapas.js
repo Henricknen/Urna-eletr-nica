aa5f324bffbb8943ba577342b018a485760750ff
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-25 19:16:50 -0300

    criando_arquivo_'etapas.js'_para_iniciar_a_codificacao_do_script_com_mais_clareza

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+    {
+        titulo: 'FRAMEWORK',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 45156,
+                nome: 'Vue.js',
+                categoria: 'frontend',
+                foto:[
+                    {url:'vue.jpg', legenda:''}
+                ]
+            },
+            {
+                numero: 54320,
+                nome: 'Node.js',
+                categoria: 'backend',
+                foto:[
+                    {url:'node.jpg', legenda:''}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'LINGUAGEM DE PROGRAMAÇÃO',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 1,
+                nome: 'JavaScript',
+                categoria: 'frontend',
+                foto:[
+                    {url:'js.jpg', legenda:''}
+                ]
+            },
+            {
+                numero: 2,
+                nome: 'Python',
+                categoria: 'frontend',
+                foto:[
+                    {url:'python.jpg', legenda:''}
+                ]
+            },
+        ]
+    }
+]
