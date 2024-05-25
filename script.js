ae36a23bd048a9bea26fc66ff871905634550197
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-05-25 19:32:48 -0300

    'selecionando'_os_elementos_do_do_arquivo_html_via_script_para_se_obter_controle_da_tela

---
+++ script.js
@@ -0,0 +1,6 @@
+let SeuVotoPara = document.querySelector('.d-1-1 span');        // seleçionando o span do elemento de class 'd-1-1' e armazendo na variável 'SeuVotoPara'
+let cat = document.querySelector('.d-1-2');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
